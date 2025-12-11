a50ca3875c7e46434f74c05737bfe8e295f3a8bc
danildperez04 <gamerdan589@gmail.com>
2025-12-11 14:23:36 -0600

    feat: add menu items

--- 03-calculadora-propinas/src/App.tsx
+++ 03-calculadora-propinas/src/App.tsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import { menuItems } from "./data/db";
+import { MenuItem } from "./components/MenuItem";
+
 function App() {
+  const [items, setItems] = useState(menuItems);
 
   return (
     <>
-      Hola mundo
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Calculadora de propinas y consumo</h1>
+      </header>
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menu</h2>
+          <div className="space-y-5">
+
+            {
+              items.map(item => (
+                <MenuItem
+                  key={item.id}
+                  item={item}
+                />
+              ))
+            }
+          </div>
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
